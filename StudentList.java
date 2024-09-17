5a827ff458c9fa3447f73813ac7e8ede583e0a7b
Quý <150776876+VanQuy07@users.noreply.github.com>
2024-09-17 16:01:02 +0700

    Update StudentList.java

--- StudentList.java
+++ StudentList.java
@@ -1,44 +1,66 @@
-
-package baitap;
+package processor;
 
 import java.util.ArrayList;
 
-
 public class StudentList {
-    ArrayList<Student> studentList = new ArrayList<>();
-    public void addStudent(Student student)
-   
-    {
+
+    private ArrayList<Student> studentList = new ArrayList<>();
+
+    public void addStudent(Student student) {
         studentList.add(student);
-    }    
-    public Student findStudentById(String id){
-        for (Student student : studentList){
-            if (student.getId().equals(id)){
+    }
+
+    public Student findStudentById(String id) {
+        for (Student student : studentList) {
+            if (student.getId().equals(id)) {
                 return student;
             }
         }
         return null;
     }
-    public void deleteStudentById(String id){
-        studentList.removeIf(student ->student.getID().equals(id));
+
+    public void deleteStudentById(String id) {
+        studentList.removeIf(student -> student.getId().equals(id));
     }
-    public void displayAllStudents(){
-        for (Student student : studentList){
+
+    public void displayAllStudents() {
+        for (Student student : studentList) {
             student.displayInfo();
-            
+            System.out.println("-----------------------------");
         }
     }
-    public Student findTopStudent(){
-        if (studentList.isEmpty()){
+public void updateStudentById(String id, Student newStudentData) {
+        for (int i = 0; i < studentList.size(); i++) {
+            if (studentList.get(i).getId().equals(id)) {
+                studentList.set(i, newStudentData);
+                return;
+            }
+        }
+        System.out.println("Student with ID " + id + " not found.");
+    }
+    public Student findTopStudent() {
+        if (studentList.isEmpty()) {
             return null;
+        }
+
+        Student topStudent = studentList.get(0);
+        for (Student student : studentList) {
+            if (student.getGpa() > topStudent.getGpa()) {
+                topStudent = student;
+            }
+        }
+        return topStudent;
     }
-    Student topStudent = studentList.get(0);
-    for (Student student : studentList){
-    if (student.getGPA()>topStudent.getGPA()){
-        topStudent = student;
+
+    public void displayStudentsWithScholarship() {
+        System.out.println("Students with Scholarship:");
+        for (Student student : studentList) {
+            if (student.getGpa() >= 4) {
+                student.displayInfo();
+            }
+        }
+        System.out.println("-----------------------------");
     }
+
+   
 }
-    return topStudent;
-    }   
-}
-        
